13b19fd5674003512366a9a947f288bd2e9de141
Blankkk <mishasokolov2709@gmail.com>
2024-01-22 13:32:44 +0100

    Task001 - Carousel.js imports

---
+++ online_shop/src/components/Carousel.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom/client";
+import { Carousel } from 'react-responsive-carousel';
+
+class Carousel extends Component {
+    render() {
+        return (
+            <Carousel>
+                <div>
+                    
+                </div>
+            </Carousel>
+        )
+    }
+}
