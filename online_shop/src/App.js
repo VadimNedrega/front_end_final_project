05ef18680d85d698042090dde7683fdb7e2a7ccd
Blankkk <mishasokolov2709@gmail.com>
2024-01-26 13:24:22 +0100

    Task001 - deleted .idea

--- online_shop/src/App.js
+++ online_shop/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Carousel } from './components/Carousel/Carousel.jsx';
+import { Carousel } from './components/Carousel/Carousel.jsx'; //TODO cпросить в пн у сереги почему експорт не работает
 
 export function App () {
         return (<div>
