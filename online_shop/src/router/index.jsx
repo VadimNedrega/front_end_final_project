6b2fbf4181ddc4fb8f45a4c2a06197525b9cc2e6
litos-ua <andrzejlitkiewicz@gmail.com>
2024-02-01 15:54:37 +0100

    f_project_header_07 (second_router_with_pictures)

--- online_shop/src/router/index.jsx
+++ online_shop/src/router/index.jsx
@@ -1,27 +1,9 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import { App } from '../App';
-// import Products from "../pages/products";
-//
-//
-// export const AppRouter = () => {
-//
-//     return (
-//         <Router>
-//             <Routes>
-//                 <Route path="/" element={<App />} />
-//                 <Route
-//                     path="/products" element={<Products />}
-//                 />
-//             </Routes>
-//         </Router>
-//     );
-// };
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { App } from '../App';
-import Products from '../pages/products';
+import { Products } from '../pages/products';
+import { Cart } from '../pages/cart'
 
 export const AppRouter = () => {
     return (
@@ -30,6 +12,7 @@ export const AppRouter = () => {
                 <Route path="/" element={<App />} />
                 <Route path="/products" element={<Products />} />
                 <Route path="/products/:productKey" element={<Products />} />
+                <Route path="/cart" element={<Cart />} />
             </Routes>
         </Router>
     );
