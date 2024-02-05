42b5be3469b12973cfbe923454d10d4566c0b2e0
Vadim Nedrega <vadim.nedrega@gmail.com>
2024-02-05 19:43:28 +0200

    TASK_001 added routes for Footer's first column

--- online_shop/src/components/LoginForm/LoginForm.jsx
+++ online_shop/src/components/LoginForm/LoginForm.jsx
@@ -2,38 +2,44 @@ import React from "react";
 import "./LoginForm.css"
 import {FaUser, FaLock} from "react-icons/fa";
 import {HomeButton} from "../HomeButton";
+import {Header} from "../Header";
+import {Footer} from "../Footer";
 
 export function LoginForm() {
     const buttonName = "Login";
 
     return (
-        <div className="wrapper-container">
-            <div className="wrapper">
-                <form action="">
-                    <h1>Login</h1>
-                    <div className="input-box">
-                        <input type="text" placeholder="Username" required/>
-                        <FaUser className="icon"/>
-                    </div>
-                    <div className="input-box">
-                        <input type="password" placeholder="Password" required/>
-                        <FaLock className="icon"/>
-                    </div>
+        <div>
+            <Header/>
+            <div className="wrapper-container">
+                <div className="wrapper">
+                    <form action="">
+                        <h1>Login</h1>
+                        <div className="input-box">
+                            <input type="text" placeholder="Username" required/>
+                            <FaUser className="icon"/>
+                        </div>
+                        <div className="input-box">
+                            <input type="password" placeholder="Password" required/>
+                            <FaLock className="icon"/>
+                        </div>
 
-                    <div className="remember-forgot">
-                        <label><input type="checkbox"/>Remember me </label>
-                        <a href="#"> Forgot password?</a>
-                    </div>
+                        <div className="remember-forgot">
+                            <label><input type="checkbox"/>Remember me </label>
+                            <a href="#"> Forgot password?</a>
+                        </div>
 
-                    <button type="submit">
-                        <HomeButton buttonName={buttonName}/>
-                    </button>
+                        <button type="submit">
+                            <HomeButton buttonName={buttonName}/>
+                        </button>
 
-                    <div className="register-link">
-                        <p>Don't have an account? <a href="/registration">Register</a></p>
-                    </div>
-                </form>
+                        <div className="register-link">
+                            <p>Don't have an account? <a href="/registration">Register</a></p>
+                        </div>
+                    </form>
+                </div>
             </div>
+            <Footer/>
         </div>
     )
 }
--- online_shop/src/router/index.jsx
+++ online_shop/src/router/index.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { App } from '../App';
 import { Products } from '../pages/products';
 import { Cart } from '../pages/cart'
-import {Header, LoginForm, Footer, RegistrationForm} from "../components";
+import {LoginForm, RegistrationForm, DeliveryPage, WarrantyPage, LoyaltyPage, GiftCardsPage} from "../components";
 
 export const AppRouter = () => {
     return (
@@ -13,16 +13,12 @@ export const AppRouter = () => {
                 <Route path="/products" element={<Products />} />
                 <Route path="/products/:productKey" element={<Products />} />
                 <Route path="/cart" element={<Cart />} />
-                <Route path="/login" element={<div>
-                    <Header/>
-                    <LoginForm/>
-                    <Footer/>
-                </div>} />
-                <Route path="/registration" element={<div>
-                    <Header/>
-                    <RegistrationForm/>
-                    <Footer/>
-                </div>} />
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/registration" element={<RegistrationForm />} />
+                <Route path="/delivery" element={<DeliveryPage />} />
+                <Route path="/warranty" element={<WarrantyPage />} />
+                <Route path="/loyalty" element={<LoyaltyPage />} />
+                <Route path="/giftCards" element={<GiftCardsPage />} />
             </Routes>
         </Router>
     );
