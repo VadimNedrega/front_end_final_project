359b477b7599cced0ac4320c7448abc9e3befdbb
litos-ua <andrzejlitkiewicz@gmail.com>
2024-02-09 18:12:50 +0100

    f_project_17  (create image path config)

--- online_shop/src/App.js
+++ online_shop/src/App.js
@@ -2,19 +2,10 @@ import React from "react";
 import { Header }  from "./components"
 import { Carousel } from './components';
 import { Footer } from "./components";
+import {images} from "./resources";
 
 export function App () {
-    const images = [
-        'https://klassmarket.ua/image/catalog/data/home/home_slider/001/001.jpg',
-        'https://klassmarket.ua/image/catalog/data/home/home_slider/001/002.jpg',
-        'https://klassmarket.ua/image/catalog/data/home/home_slider/001/003.jpg',
-        'https://klassmarket.ua/image/catalog/data/home/home_slider/001/004.jpg',
-        'https://klassmarket.ua/image/catalog/data/home/home_slider/001/005.jpg',
-        'https://klassmarket.ua/image/catalog/data/home/home_slider/001/006.jpg',
-        'https://klassmarket.ua/image/catalog/data/home/home_slider/001/007.jpg',
-        'https://klassmarket.ua/image/catalog/data/home/home_slider/001/008.jpg',
-        'https://klassmarket.ua/image/catalog/data/home/home_slider/001/009.jpg',
-        ];
+
     return (<div>
         <Header />
         <Carousel images={images} />
---
+++ online_shop/src/resources/index.js
@@ -0,0 +1,2 @@
+export * from "./path_carousel_img";
+export * from "./range_of_products";
