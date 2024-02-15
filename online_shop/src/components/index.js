2b4956adfe1947ea4289e4238cf760df5e915112
Blankkk <mishasokolov2709@gmail.com>
2024-02-15 13:55:49 +0100

    Task 001 - not working code cleared

--- online_shop/src/components/index.js
+++ online_shop/src/components/index.js
@@ -2,6 +2,3 @@ export * from "./Footer";
 export * from "./Header";
 export * from "./Carousel";
 export * from "./HomeButton";
-export * from "./Card";
-export * from "./Card/MainComponent"
-export * from "./Card/CardComponent"
