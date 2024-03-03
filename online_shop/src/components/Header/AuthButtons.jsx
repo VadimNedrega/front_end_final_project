b5f5a68da54c2e9470870a77da752e661707df63
Vadim Nedrega <vadim.nedrega@gmail.com>
2024-03-03 11:30:55 +0200

    Login with Redux & localStorage

--- online_shop/src/components/Header/AuthButtons.jsx
+++ online_shop/src/components/Header/AuthButtons.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import {ROUTE} from '../../router';
-import { useNavigate  } from 'react-router-dom';
+import { ROUTE } from '../../router';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {logoutSuccess, selectIsAuthenticated} from '../../ducks/login.actions';
 
 export function AuthButtons() {
+    const dispatch = useDispatch();
+    const isAuthenticated = useSelector(selectIsAuthenticated);
     const navigate = useNavigate();
 
     const handleSignIn = () => {
@@ -13,14 +17,27 @@ export function AuthButtons() {
         navigate(ROUTE.REGISTRATION);
     };
 
+    const handleLogOut = () => {
+        dispatch(logoutSuccess());
+    };
+
     return (
         <div className="authbuttons">
-            <button className="header__btn header__btn_common header__btn_primary" onClick={handleSignIn}>
-                Увійти
-            </button>
-            <button className="header__btn header__btn_common header__btn_third" onClick={handleSignUp}>
-                Реєстрація
-            </button>
+            {!isAuthenticated && (
+                <>
+                    <button className="header__btn header__btn_common header__btn_primary" onClick={handleSignIn}>
+                        Увійти
+                    </button>
+                    <button className="header__btn header__btn_common header__btn_third" onClick={handleSignUp}>
+                        Реєстрація
+                    </button>
+                </>
+            )}
+            {isAuthenticated && (
+                <button className="header__btn header__btn_common header__btn_third" onClick={handleLogOut}>
+                    Logout
+                </button>
+            )}
         </div>
     );
 }
--- online_shop/src/services/store.js
+++ online_shop/src/services/store.js
@@ -1,13 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterSlice from '../ducks/counter';
-import cartReducer from '../ducks/cart.duck'
+import counterReducer from '../ducks/counter';
+import cartReducer from '../ducks/cart.duck';
+import authReducer from '../ducks/login.actions';
+import { loadState, saveState } from './localStorage';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    counter: counterReducer,
+    cart: cartReducer,
+    auth: authReducer,
+});
+
+const preloadedState = loadState();
 
 export const store = configureStore({
-    reducer: {
-        counter: counterSlice,
-        cart: cartReducer,
-    },
+    reducer: rootReducer,
+    preloadedState,
     devTools: true,
+});
+
+store.subscribe(() => {
+    saveState(store.getState());
 });
 
 export default store;
