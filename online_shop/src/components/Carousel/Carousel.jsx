eb04347c57b6a77638c1dcf6acb366ee4ed9d96c
Vadim Nedrega <vadim.nedrega@gmail.com>
2024-03-11 11:05:19 +0200

    removed buttons

--- online_shop/src/components/Carousel/Carousel.jsx
+++ online_shop/src/components/Carousel/Carousel.jsx
@@ -10,11 +10,6 @@ export const Carousel = ({images, interval = 3500}) => {
             prevIndex === images.length - 1 ? 0 : prevIndex + 1
         );
     };
-    // const prevSlide = () => {
-    //     setActiveIndex((prevIndex) =>
-    //         prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    //     );
-    // };
 
     //Adds autoscroll to the slider
     useEffect(() => {
@@ -27,16 +22,10 @@ export const Carousel = ({images, interval = 3500}) => {
     return (
         <div className={"carousel__container"}>
             <div className={"carousel"}>
-                {/*<button onClick={prevSlide} className={"carousel__btn carousel__btn--prev"}>*/}
-                {/*    &lt;*/}
-                {/*</button>*/}
                 <img src={images[activeIndex]}
                      alt={`Slide ${activeIndex + 1}`}
                      className={"carousel__img"}
                 />
-                {/*<button onClick={nextSlide} className={"carousel__btn carousel__btn--next"}>*/}
-                {/*    &gt;*/}
-                {/*</button>*/}
             </div>
         </div>
     );
